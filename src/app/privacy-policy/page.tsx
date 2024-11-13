7f055136904d49a87fa555308bd49dbe2b0d8e73
Ridham <ridhamavaiya1999@gmail.com>
2024-11-13 16:56:53 +0530

    updated sitemap xml

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -169,7 +169,7 @@ export default async function PrivacyPolicy() {
       <h4 className="mt-2 text-[18px] font-semibold ">Usage Data</h4>
       <p>Usage Data is collected automatically when using the Service.</p>
       <p>
-        Usage Data may include information such as Your Device's Internet
+        Usage Data may include information such as Your Device&apos;s Internet
         Protocol address (e.g. IP address), browser type, browser version, the
         pages of our Service that You visit, the time and date of Your visit,
         the time spent on those pages, unique device identifiers and other
@@ -311,7 +311,7 @@ export default async function PrivacyPolicy() {
           <p>
             <strong>To contact You:</strong> To contact You by email, telephone
             calls, SMS, or other equivalent forms of electronic communication,
-            such as a mobile application's push notifications regarding updates
+            such as a mobile application&apos;s push notifications regarding updates
             or informative communications related to the functionalities,
             products or contracted services, including the security updates,
             when necessary or reasonable for their implementation.
@@ -411,7 +411,7 @@ export default async function PrivacyPolicy() {
         Transfer of Your Personal Data
       </h3>
       <p>
-        Your information, including Personal Data, is processed at the Company's
+        Your information, including Personal Data, is processed at the Company&apos;s
         operating offices and in any other places where the parties involved in
         the processing are located. It means that this information may be
         transferred to — and maintained on — computers located outside of Your
@@ -496,7 +496,7 @@ export default async function PrivacyPolicy() {
         means to protect Your Personal Data, We cannot guarantee its absolute
         security.
       </p>
-      <h2 className="text-[20px] font-bold mt-5">Children's Privacy</h2>
+      <h2 className="text-[20px] font-bold mt-5">Children&apos;s Privacy</h2>
       <p>
         Our Service does not address anyone under the age of 13. We do not
         knowingly collect personally identifiable information from anyone under
@@ -509,14 +509,14 @@ export default async function PrivacyPolicy() {
       <p>
         If We need to rely on consent as a legal basis for processing Your
         information and Your country requires consent from a parent, We may
-        require Your parent's consent before We collect and use that
+        require Your parent&apos;s consent before We collect and use that
         information.
       </p>
       <h2 className="text-[20px] font-bold mt-5">Links to Other Websites</h2>
       <p>
         Our Service may contain links to other websites that are not operated by
         Us. If You click on a third party link, You will be directed to that
-        third party's site. We strongly advise You to review the Privacy Policy
+        third party&apos;s site. We strongly advise You to review the Privacy Policy
         of every site You visit.
       </p>
       <p>
--- src/app/terms-conditions/page.tsx
+++ src/app/terms-conditions/page.tsx
@@ -184,7 +184,7 @@ export default async function TermsAndCondition() {
         of the Company and any of its suppliers under any provision of this
         Terms and Your exclusive remedy for all of the foregoing shall be
         limited to the amount actually paid by You through the Service or 100
-        USD if You haven't purchased anything through the Service.
+        USD if You haven&apos;t purchased anything through the Service.
       </p>
       <p>
         To the maximum extent permitted by applicable law, in no event shall the
@@ -203,7 +203,7 @@ export default async function TermsAndCondition() {
         Some states do not allow the exclusion of implied warranties or
         limitation of liability for incidental or consequential damages, which
         means that some of the above limitations may not apply. In these states,
-        each party's liability will be limited to the greatest extent permitted
+        each party&apos;s liability will be limited to the greatest extent permitted
         by law.
       </p>
       <h2 className="text-[20px] font-bold mt-5">
@@ -230,7 +230,7 @@ export default async function TermsAndCondition() {
       </p>
       <p>
         Without limiting the foregoing, neither the Company nor any of the
-        company's provider makes any representation or warranty of any kind,
+        company&apos;s provider makes any representation or warranty of any kind,
         express or implied: (i) as to the operation or availability of the
         Service, or the information, content, and materials or products included
         thereon; (ii) that the Service will be uninterrupted or error-free;
@@ -291,7 +291,7 @@ export default async function TermsAndCondition() {
       <p>
         Except as provided herein, the failure to exercise a right or to require
         performance of an obligation under these Terms shall not affect a
-        party's ability to exercise such right or require such performance at
+        party&apos;s ability to exercise such right or require such performance at
         any time thereafter nor shall the waiver of a breach constitute a waiver
         of any subsequent breach.
       </p>
@@ -307,7 +307,7 @@ export default async function TermsAndCondition() {
       <p>
         We reserve the right, at Our sole discretion, to modify or replace these
         Terms at any time. If a revision is material We will make reasonable
-        efforts to provide at least 30 days' notice prior to any new terms
+        efforts to provide at least 30 days&apos; notice prior to any new terms
         taking effect. What constitutes a material change will be determined at
         Our sole discretion.
       </p>
